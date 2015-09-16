f2365f1cdcea0fe71aec2b84a7169ac6797fb996
ltinter <5201249@qq.com>
2015-09-16 12:00:36 +0800

    :octocat: Added .gitattributes

---
+++ 001_TwoSum/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _001_TwoSum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtMain = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(12, 39);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "GO";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtMain
+            // 
+            this.txtMain.Location = new System.Drawing.Point(12, 12);
+            this.txtMain.Name = "txtMain";
+            this.txtMain.Size = new System.Drawing.Size(373, 21);
+            this.txtMain.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 421);
+            this.Controls.Add(this.txtMain);
+            this.Controls.Add(this.btnGo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtMain;
+    }
+}
+
---
+++ 002_AddTwoNumbers/Form1.cs
@@ -0,0 +1,82 @@
+﻿//You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+//Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+//Output: 7 -> 0 -> 8
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _002_AddTwoNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] intA = new int[] { 1 };
+            int[] intB = new int[] { 9, 9 };
+            ListNode lnA = PushNodes(intA), lnB = PushNodes(intB);
+
+            ListNode Result = (new Solution()).AddTwoNumbers(lnA, lnB);
+        }
+
+        private ListNode PushNodes(int[] DataToPush)
+        {
+            ListNode InitNode = new ListNode(DataToPush[0]);
+            ListNode tempNode = InitNode;
+            for (int i = 1; i < DataToPush.Length; i++)
+            {
+                tempNode.next = new ListNode(DataToPush[i]);
+                tempNode = tempNode.next;
+            }
+            return InitNode;
+        }
+
+        public class Solution
+        {
+            public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+            {
+                ListNode EmptyNode = new ListNode(0);
+                ListNode ReturnLN = new ListNode((l1.val + l2.val) % 10);
+                ListNode CurrentNode1 = l1, CurrentNode2 = l2, CurrentNode = ReturnLN;
+                bool Carry = l1.val + l2.val > 9;
+
+                while ((CurrentNode1 != null && CurrentNode1.next != null) || (CurrentNode2 != null && CurrentNode2.next != null))
+                {
+                    int IntA, IntB;
+                    if (CurrentNode1 == null || CurrentNode1.next == null) { IntA = 0; } else { IntA = CurrentNode1.next.val; }
+                    if (CurrentNode2 == null || CurrentNode2.next == null) { IntB = 0; } else { IntB = CurrentNode2.next.val; }
+                    int tempSum = IntA + IntB + (Carry ? 1 : 0);
+                    CurrentNode.next = new ListNode(tempSum % 10);
+                    Carry = tempSum > 9;
+                    CurrentNode = CurrentNode.next == null ? null : CurrentNode.next;
+                    CurrentNode1 = CurrentNode1 == null || CurrentNode1.next == null ? null : CurrentNode1.next;
+                    CurrentNode2 = CurrentNode2 == null || CurrentNode2.next == null ? null : CurrentNode2.next;
+                }
+
+                if (Carry)
+                {
+                    CurrentNode.next = new ListNode(1);
+                }
+                return ReturnLN;
+            }
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
